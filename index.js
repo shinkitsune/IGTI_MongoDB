bdf2825e0b659b0e537f47177da1e2ce9c9c53e2
Elton <eltonandradee@gmail.com>
2020-08-30 01:32:28 -0300

    create async for connection the database

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import mongoose from "mongoose";
+
+
+
+(async () => {
+  try {
+    await mongoose.connect(url, {
+      // await mongoose.connect('mongodb+srv://adminbootcamp:acessdbbootcamp@cluster0.rua7b.mongodb.net/account_bank?retryWrites=true&w=majority', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    });
+    console.log("Conectado com sucesso ao banco de dados!");
+  } catch (error) {
+    console.log("Erro ao conectar com o banco de dados: ", error);
+  }
+})();
